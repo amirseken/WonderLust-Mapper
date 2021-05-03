890ff7386a6addaeafc113640eb5fe7264cad014
Amir Seken <74365816+amirseken@users.noreply.github.com>
2021-05-04 00:01:00 +0600

    Create m1_helper_functions.h

---
+++ m1_helper_functions.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   m1_helper_functions.h
+ * Author: kambrisr
+ *
+ * Created on April 1, 2021, 9:41 AM
+ */
+
+#pragma once 
+
+
+
+
+std::string appropriate_font(); 
+
