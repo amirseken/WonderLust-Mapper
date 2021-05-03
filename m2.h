f5d0a0bd84394ebbe21328ef197fadd051da75da
Amir Seken <74365816+amirseken@users.noreply.github.com>
2021-05-04 00:02:42 +0600

    Create m2.h

---
+++ m2.h
@@ -0,0 +1,16 @@
+
+#pragma once
+//
+//// Draws the currently loaded map.
+//// draw_map () assumes the map has already been loaded with loadMap ()
+//// before it is called. The ece297exercise unit tests always call 
+//// loadMap (string map_name) before calling draw_map. Your main () program
+//// should follow the same convention.
+
+
+
+
+
+void draw_map();
+
+
