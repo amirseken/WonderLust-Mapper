79801b28af90e14546d68cd7eb8c53f58c7260da
Amir Seken <74365816+amirseken@users.noreply.github.com>
2021-05-04 00:09:12 +0600

    Create m4_helper.cpp

---
+++ m4_helper.cpp
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+#include <vector> 
+//void finding_legal_paths(const std::vector <DeliveryInf> & deliveries, const float turn_penalty);
+
+    
+    
+
+
+
+//void helper_2opt(std::vector<StreetSegmentIdx>& shortest_delivery_path, const float turn_penalty);
